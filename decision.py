ebf031a17f73e3d16b357c3a050a34a43f212c1f
Ryan Ericson <ryan.ericson@my.jcu.edu.au>
2025-12-01 15:21:28 +1000

    Added the kebab calculatoer taht takes in size and amount of topping then passes to output

--- decision.py
+++ decision.py
@@ -5,20 +5,54 @@ You could two versions - one with nested ifs and one with the "and" operator.
 """
 
 
-user_age = int(input("Age: "))
-enrolled_to_vote = input("Enrolled: ")
+# user_age = int(input("Age: "))
+# enrolled_to_vote = input("Enrolled: ")
 
 
-""" Nested if's """
-# if user_age >= 18:
+# """ Nested if's """
+# # if user_age >= 18:
+# #     print("You can vote")
+# # else:
+# #     print("You cannot vote")       
+
+
+# """ "AND" """
+
+# if user_age >= 18 and enrolled_to_vote == "yes":
 #     print("You can vote")
 # else:
-#     print("You cannot vote")       
+#     print("You cannot vote")
+    
 
+""" 
+Write the Kebab Kalculator. The program should have base prices for small ($7.50) or large ($10) kebabs (constants)
+The user should choose a size, and how many extra toppings they want: up to 2 toppings are free, 3 or more toppings are 50c each (but not counting the free ones!).
+Examples:
+a small kebab with 3 toppings = $8
+a large kebab with 2 toppings = $10
+large with 10 = $14
+Ask the user for their options, then print the final price.
+"""
 
-""" "AND" """
+S_KEBAB = 7.50
+L_KEBAB = 10
 
-if user_age >= 18 and enrolled_to_vote == "yes":
-    print("You can vote")
-else:
-    print("You cannot vote")
+toppings_price = 0.50
+
+choice = input("Size: S or L: ")
+quant_of_top = int(input("Quantity of Toppings: "))
+
+if choice == "s":
+    if quant_of_top >= 3:
+        x = (quant_of_top - 2) * toppings_price
+        print(S_KEBAB + x)
+    else:
+        print(S_KEBAB)
+
+if choice == "l":
+    if quant_of_top >= 3:
+        x = (quant_of_top - 2) * toppings_price
+        print(L_KEBAB + x)
+    else:
+        print(L_KEBAB)
+    
