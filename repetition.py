ba048d5d806fc628c51fa51ab5fae80ffdf58a02
Ryan Ericson <ryan.ericson@my.jcu.edu.au>
2025-12-16 16:39:40 +1000

    Completed squaring of numbers within a given range.

--- repetition.py
+++ repetition.py
@@ -21,24 +21,38 @@ while choice != <quit option>
     get choice
 <do final thing, if needed>
 """
-name = input("Name: ")
-menu = "(G)et name, \n(U)ppercase,\n(L)owercase,\n(Q)uit" 
-print(menu)
+# name = input("Name: ")
+# menu = "(G)et name, \n(U)ppercase,\n(L)owercase,\n(Q)uit" 
+# print(menu)
+# 
+# choice = str(input("choice: "))
+# while choice != "q":
+    # if choice == "g":
+        # name = input("name")
+        # print(f"Your new name{name}")
+    # elif choice == "u":
+        # print(name.upper())
+    # elif choice == "l":
+        # name == name.lower()
+        # print(name)
+    # else:
+        # print("Choose again")
+    # print(menu)
+    # choice = input("choice: ")
+# print(name)
+    # 
+    
 
-choice = str(input("choice: "))
-while choice != "q":
-    if choice == "g":
-        name = input("name")
-        print(f"Your new name{name}")
-    elif choice == "u":
-        print(name.upper())
-    elif choice == "l":
-        name == name.lower()
-        print(name)
-    else:
-        print("Choose again")
-    print(menu)
-    choice = input("choice: ")
-print(name)
-    
-    
+""" Print the square of numbers between a given range. Ask the user for a lower number and a higher number, then print all of the squares of the numbers in that range (inclusive).
+Error-check the second number to make sure it is higher than the first. """
+
+low_num = int(input("Low Number: "))
+high_num = int(input("High Number: "))
+
+while low_num > high_num:
+    low_num = int(input("Try again: "))
+else:
+    # y = low_num * low_num
+    for x in range(high_num):
+        print(x*low_num)
+    # print(x)
