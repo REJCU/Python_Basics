a0e43678cca49eb14362f1c46a6e7fb3f77aa341
Ryan Ericson <ryan.ericson@my.jcu.edu.au>
2025-12-16 18:46:02 +1000

    Created a function page

---
+++ functions.py
@@ -0,0 +1,5 @@
+"""
+Write a function that calculates which of two products is cheaper based on a quantity and price. 
+The function will take 4 parameters - the quantity and price of each product, and output either "First" or "Second".
+"""
+
