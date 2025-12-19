84d251a69c78f36a050822114fba35eeae0ffb18
Ryan Ericson <ryan.ericson@my.jcu.edu.au>
2025-12-19 19:59:33 +1000

    Able to input name and phone number to dictionary. Able to delete dictionary inputs.

--- lists_and_disctionaries.py
+++ lists_and_disctionaries.py
@@ -72,32 +72,76 @@ Modify the word-counting program (from File I/O)
 so that the user can enter an arbitrary number of words and it prints the number of times each of those words occurs
 """
 
-word_store = {}
-
-file_name = "scores.txt"
-word = input("Enter a words: ")
-
-with open( "random_words.txt", "w") as f:
-        f.write(word)
-
-with open("random_words.txt", "r") as rwords:
-    contents = rwords.read().split()
-    word_count = contents.count(word)
-    print("Word count: ", word_count, contents)
-
-    only_words = [content.strip().lower() for content in contents]
-    print("Only words: ", only_words)
-
-    for answer in only_words:
-        word_store[answer] = word_store.get(answer, 0) + 1
-        print("Answer: ", answer, word_store[answer])
-
-#         only_letters = [content.strip().upper() for content in contents if content.isalpha()]
-
-        # for answer in only_letters:
-        #     storage[answer] = storage.get(answer, 0) + 1
-        #     sorted_results = sorted(storage.items(), key=lambda item: item[1], reverse=True)
+# word_store = {}
+#
+# file_name = "scores.txt"
+# word = input("Enter a words: ")
+#
+# with open( "random_words.txt", "w") as f:
+#         f.write(word)
+#
+# with open("random_words.txt", "r") as rwords:
+#     contents = rwords.read().split()
+#     word_count = contents.count(word)
+#
+#     only_words = [content.strip().lower() for content in contents]
+#
+#     for answer in only_words:
+#         word_store[answer] = word_store.get(answer, 0) + 1
+#
+#     for x, y in word_store.items():
+#         print("Word: ",x, " - Times Appeared ", y)
 
 
-#         for letter, count in sorted_results:
-#             print(f"{letter}: {count}")
+"""
+Write a program to store your phone book in a dictionary. Give the user choices (menu) for adding, deleting, modifying and searching for entries.
+"""
+
+phone_book = {}
+MENU = "(A)dding\n(D)eleting\n(M)odifying\n(S)earching\n(Q)uit"
+
+
+print(MENU)
+user_choice = input("What would you like to do? ").upper()
+while user_choice != "Q":
+    if user_choice == "A":
+        name= input("What is your name? ")
+        phone = input("What is your phone number? ")
+        phone_book[name] = phone
+        print("Added: ",name," has " ,phone_book[name])
+    user_choice = input("What would you like to do? ").upper()
+
+    if user_choice == "D":
+        print(phone_book)
+        deleted_user = input("Delete user: ")
+        if deleted_user in phone_book:
+            del phone_book[deleted_user]
+            print(phone_book)
+        else:
+            print("Sorry, that user does not exist.")
+
+
+    if user_choice == "M":
+        pass
+    if user_choice == "S":
+        pass
+    if user_choice == "Q":
+        break
+    print(MENU)
+    user_choice = input("What would you like to do? ").upper()
+
+# display menu
+# get choice
+# while choice != <quit option>
+#     if choice == <first option>
+#         <do first task>
+#     else if choice == <second option>
+#         <do second task>
+#     ...
+#     else if choice == <n-th option>
+#         <do n-th task>
+#     else
+#         display invalid input error message
+#     display menu
+#     get choice
+# <do final thing, if needed>
