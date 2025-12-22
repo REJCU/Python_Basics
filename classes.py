e737b66473ab32e4d2d7255ec2ee46e3a7eb42f8
Ryan Ericson <ryan.ericson@my.jcu.edu.au>
2025-12-22 20:09:44 +1000

    completed recursive functions for counting down

--- classes.py
+++ classes.py
@@ -10,22 +10,44 @@ and what methods might be useful.
     clothing item
 """
 
-class Computer:
+# class Computer:
 
-    sticks = []
+#     sticks = []
 
-    def __init__(self, ram, gpu, ssd):
-        self.ram = ram
-        self.gpu = gpu
-        self.ssd = ssd
+#     def __init__(self, ram, gpu, ssd):
+#         self.ram = ram
+#         self.gpu = gpu
+#         self.ssd = ssd
 
-    def add_ram(self, stick):
-        self.sticks.append(stick)
-
-e = Computer("ddr4","luna","1tb")
-
-print(e.gpu)
-e.add_ram("ddr5")
-print(e.sticks)
+#     def add_ram(self, stick):
+#         self.sticks.append(stick)
 
 
+# e = Computer("ddr4","luna","1tb")
+
+# print(e.gpu)
+# e.add_ram("ddr5")
+# print(e.sticks)
+# swap_ssd = input("Swap to: ")
+# e.change_ssd("hdd")
+
+
+class Product:
+    products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True], ...]
+
+    on_sale_products = []
+
+    def __init__(self, product, price, on_sale):
+        self.product = str
+        self.price = int
+        self.on_sale = on_sale
+    
+    def print_sales(self):
+        with open("product_list.txt", "r") as r:
+            r = r.read()
+            print(r)
+
+    
+
+e = Product("Toy", 350, True)
+e.print_sales()
---
+++ recursion.py
@@ -0,0 +1,33 @@
+"""
+Write a recursive function that prints all of the numbers from a given input down to 0.
+"""
+
+# def going_down(x):
+#     # if (x>0):
+#     #     result = going_down(x - 1) + 1 
+#     #     print(result)
+#     # else:
+#     #     result = 0 
+#     # return result
+#     print(x)
+#     if x == 0:
+#          return 
+#     else:
+#          result = going_down(x - 1)
+#     return result
+
+# going_down(100)
+
+
+"""
+Write a recursive function that displays all of the letters in a string. 
+(Each function call displays one letter.)
+"""
+
+def make_letter_string(x):
+    print(x)
+    if 
+
+
+user_submitt = input("Word: ")
+make_letter_string()
